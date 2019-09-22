23e4295fd7ba82b8a37a81d062574920c3f1bd71
Collin Smith <cmsmith.tx@gmail.com>
2019-09-22 13:43:48 -0500

    form working/styled

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,26 +9,46 @@ export class Calculator extends Component {
         loanTerm: '',
         interest: ''
     }
+
+    handleValueChange = (e) => {
+        this.setState({ homeValue : e.target.value })
+    }
+
+    handlePaymentChange = (e) => {
+        this.setState({ downPayment : e.target.value })
+    }
+
+    handlePaymentPercentChange = (e) => {
+        this.setState({ downPaymentpercent : e.target.value })
+    }
+
+    handleTermChange = (e) => {
+        this.setState({ loanTerm : e.target.value })
+    }
+
+    handleInterestChange = (e) => {
+        this.setState({ interest : e.target.value })
+    }
     
     render() {
         return (
             <div>
                 <form>
                     <label>Home Value:</label>
-                    $<input type="text" value={this.state.homeValue} />
+                    $<input type="text" value={this.state.homeValue} onChange={this.handleValueChange} />
                     <label>Down Payment:</label>
-                    $<input type="text" value={this.state.downPayment} />
+                    $<input type="text" value={this.state.downPayment} onChange={this.handlePaymentChange} />
                     <label>Down Payment Percentage:</label>
-                    %<input type="text" value={this.state.downPayment} />
+                    %<input type="text" value={this.state.downPaymentpercent} onChange={this.handlePaymentPercentChange} />
                     <label>Loan Term:</label>
-                    <select value={this.state.loanTerm} onChange="" className='term_length'>
+                    <select value={this.state.loanTerm} className='term_length' onChange={this.handleTermChange}>
                         <option value={10}>10 Year Fixed</option>
                         <option value={15}>15 Year Fixed</option>
                         <option value={20}>20 Year Fixed</option>
                         <option  value={30}>30 Year Fixed</option>
                     </select>
                     <label>Interest Percentage</label>
-                    <input type="text" value={this.state.interest} />
+                    <input type="text" value={this.state.interest} onChange={this.handleInterestChange} />
                 </form>
             </div>
         )
